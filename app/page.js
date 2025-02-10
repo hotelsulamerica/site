82ac0f17a62fc07f7c5f9deb81263af657145f90
hrm4k3r <hebertdev82@gmail.com>
2025-02-10 16:35:12 -0300

    feat: under construction page

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen text-center">
+      <div>
+        <h1 className="text-4xl font-bold">Hotel Sul América</h1>
+        <h3 className="text-xl mt-5">
+          Site em construção, propagando em até 48 horas. <span className="font-bold">Liberação em 11/02/2025, às 18 horas.</span>
+        </h3>
+      </div>
+    </div>
+  );
+}
