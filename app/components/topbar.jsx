e874014a3cbf8f0e60747c67a2d9d9f989577132
Hebert <hebertdev82@gmail.com>
2025-07-10 18:32:16 -0300

    feat: topbar desktop and mobile ok

--- app/components/topbar.jsx
+++ app/components/topbar.jsx
@@ -7,40 +7,43 @@ import Link from "next/link";
 export default function TopBar() {
   return (
     <div
-      className="flex py-2 px-16 text-sm text-white bg-[#008C44] md:relative z-10"
+      className="flex flex-col md:flex-row items-center justify-between py-2 px-16 text-sm text-white bg-[#008C44] md:relative z-10"
       style={{ boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.2)" }}
     >
-      <div className="flex-1 font-medium">
+      <div className="flex-1 font-medium mb-2 md:mb-0 text-center md:text-left">
         <p>Bem vindo ao Hotel Sul América</p>
       </div>
 
-      <div className="flex-1 md:text-right">
-        <ul className="flex justify-end space-x-8">
+      <div className="flex-1 text-center md:text-right">
+        <ul className="flex flex-col text-center md:flex-row md:justify-end md:space-x-8 md:space-y-0 space-y-2">
           <Link href="phone:3533323400" target="_blank">
-            <li className="flex font-medium">
+            <li className="flex justify-center md:justify-start font-medium hover:text-[#FFDF82] transition-colors">
               <FontAwesomeIcon
                 icon={faPhone}
-                className="font-bold text-primary w-4 h-4 mr-2 text-white"
+                className="font-bold text-primary w-4 h-4 mr-2"
               />
               (35) 3332-3400
             </li>
           </Link>
 
-          <Link href="https://api.whatsapp.com/send?phone=5535988680724&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20uma%20reserva%20no%20Hotel%20Sul%20Am%C3%A9rica" target="_blank">
-            <li className="flex font-medium">
+          <Link
+            href="https://api.whatsapp.com/send?phone=5535988680724&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20uma%20reserva%20no%20Hotel%20Sul%20Am%C3%A9rica"
+            target="_blank"
+          >
+            <li className="flex justify-center md:justify-start font-medium hover:text-[#FFDF82] transition-colors">
               <FontAwesomeIcon
                 icon={faWhatsapp}
-                className="font-bold text-primary w-4 h-4 mr-2 text-white"
+                className="font-bold text-primary w-4 h-4 mr-2"
               />
               (35) 98868-0724
             </li>
           </Link>
 
-          <Link href={"mailto:reservas@hotelsulamerica.com.br"} target="_blank">
-            <li className="flex font-medium">
+          <Link href="mailto:reservas@hotelsulamerica.com.br" target="_blank">
+            <li className="flex justify-center md:justify-start font-medium hover:text-[#FFDF82] transition-colors">
               <FontAwesomeIcon
                 icon={faEnvelope}
-                className="font-bold text-primary w-4 h-4 mr-2 text-white"
+                className="font-bold text-primary w-4 h-4 mr-2"
               />
               reservas@hotelsulamerica.com.br
             </li>
